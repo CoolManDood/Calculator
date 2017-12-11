3f83ec9beb4b5240bedfa5e5fb21174f96f22d5b
Samuel Roush <samuel.roush@warriorlife.net>
2017-12-11 14:05:32 -0800

    finished calculator, version 1.0 complete
    :+1:

--- calculator.js
+++ calculator.js
@@ -7,4 +7,15 @@ function d(val) {
 
 // Type numbers and operators
 function v(val) {
-	document.getElementById("d"
+	document.getElementById("d").value += val;
+}
+
+// Evaluate the equation
+function e() {
+	try {
+		d(eval(document.getElementById("d").value));
+	}
+	catch(err) {
+		d("Error");
+	}
+}
